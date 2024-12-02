ad0aa1394c52043e79232f92b592126b2e3a0792
raunakpurohit <raunak.wscubetech@gmail.com>
2024-12-02 19:07:06 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/prefs_341/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prefs_341
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController nameController = TextEditingController();
+  String name = "";
+  SharedPreferences? prefs;
+
+  @override
+  void initState(){
+    super.initState();
+    ///get data here
+    getData();
+
+  }
+
+  getData() async{
+    prefs = await SharedPreferences.getInstance();
+    name = prefs!.getString("name") ?? "";
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+
+      body: Column(
+        children: [
+          name!= "" ? Text("Welcome, $name", style: TextStyle(fontSize: 25),) : Container(),
+          TextField(controller: nameController,),
+          ElevatedButton(onPressed: ()async{
+            ///set data here
+            prefs!.setString("name", nameController.text);
+
+          }, child: Text('Save'))
+        ],
+      ),
+    );
+  }
+}
+
+
+class A{
+
+  A();
+}
